9525112e692bf7fff85720d63e665f9e3636ce5a
Thomas <ming100628@gmail.com>
2023-12-10 09:35:03 +0800

    fixed transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,10 +4,18 @@ class TransactionsController < ApplicationController
   end
 
   def create
+    # protect against sending money from accounts that don't belong to the current user
+    # protect against sending money to and from accounts that don't have the same currency
     receiver = Account.find_by(id: params[:recipient_account_id])
     sender = Account.find_by(id: params[:sender_account_id])
-    if (sender.balance - params[:amount].to_f) < 0
-      flash[:notice] = 'Insufficient balance'
+    if sender.currency != receiver.currency
+      flash.now[:notice] = 'Currency does not match'
+      render :new
+    elsif sender.user_id != current_user.id
+      flash.now[:notice] = 'The account does not belong to you'
+      render :new
+    elsif sender.balance < params[:amount].to_f
+      flash.now[:notice] = 'Insufficient balance'
       render :new
     else
       ActiveRecord::Base.transaction do
